dfb9c601332d15a74121d4e13e2a4f64a81c5810
Jérôme Crété <jerome1.crete@epitech.eu>
2017-11-17 16:38:03 +0100

    time

--- boggle.php
+++ boggle.php
@@ -8,6 +8,7 @@ class Boggle {
 	public $score = ['3' => 1, '4' => 1, '5' => 2, '6' => 3, '7' => 5, '8' => 11];
 	public $grid = [];
 	public $gridObj = [];
+	public $time;
 
 	public function __construct(){
 		$this->dices = self::generateDices();
