051f2f9274040f11f693f8a5ed2754adbe37150f
Olivier Crété <ocrete@sfrpims.fr>
2017-12-20 17:34:19 +0100

    messages array init

---
+++ messages.php
@@ -0,0 +1,21 @@
+<?php
+
+$messages = [
+	"welcome" => ["en" => , "fr" =>],
+	"words_found" => ["en" => , "fr" =>],
+	"enter_word" => ["en" => , "fr" =>],
+	"time_left" => ["en" => , "fr" =>],
+	"time_unit" => ["en" => , "fr" =>],
+	"elapsed_time" => ["en" => , "fr" =>],
+	"new_record" => ["en" => , "fr" =>],
+	"enter_name" => ["en" => , "fr" =>],
+	"empty" => ["en" => , "fr" =>],
+	"word_repeated" => ["en" => , "fr" =>],
+	"unexisting_word" => ["en" => , "fr" =>],
+	"word_gain" => ["en" => function($word, $score){}, "fr" =>function($word, $score){}],
+	"word_not_on_grid" => ["en" => , "fr" =>],
+	"details" => ["en" => , "fr" =>],
+];
+
+
+
